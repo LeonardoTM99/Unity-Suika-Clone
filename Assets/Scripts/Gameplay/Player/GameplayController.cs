6419801d75ac50306c0a67b6503dfdc34dba39ce
LeonardoTM99 <leonardotoma58@gmail.com>
2026-02-08 11:57:42 -0600

    feat: drop guide line & move bounds
    Implemented a visual line that shows the player the drop path the magic circle will travel.
    
    Also, when moving the magic circles (aiming), the circles will be clamped and avoid going off bounds.

--- Assets/Scripts/Gameplay/Player/GameplayController.cs
+++ Assets/Scripts/Gameplay/Player/GameplayController.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class GameplayController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Transform dropLine;
+    [SerializeField] private Transform startLine;
     private GameObject currentMagicCircle;
     private Rigidbody2D currentRb;
 
+    [Header("Parameters")]
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float timeBetweenRounds = 1.5f;
 
+    //Define the move circle limit bounds
+    float halfWidth;
+    float leftLimitX;
+    float rightLimitX;
     private float dropTimer;
     private bool isWaitingForNextRound;
     private bool canPlay;
@@ -17,20 +25,35 @@ public class GameplayController : MonoBehaviour
     private void Start()
     {
         GameStateManager.Instance.OnGameStateChanged += HandleGameStateChanged;
+
+        halfWidth = startLine.localScale.x * 0.5f;
+        leftLimitX = startLine.position.x - halfWidth;
+        rightLimitX = startLine.position.x + halfWidth;
     }
 
     private void Update()
     {
+
         if (canPlay && currentMagicCircle != null)
         {
+            //Visual Drop Line Logic
+            AppearDropLine();
+
+            //Moving magic circle logic
             float moveDir = InputManager.Instance.GetMoveInput();
 
             if (Mathf.Abs(moveDir) > 0.01f)
             {
-                currentMagicCircle.transform.position +=
-                    Vector3.right * moveDir * moveSpeed * Time.deltaTime;
+                Vector3 pos = currentMagicCircle.transform.position;
+
+                pos.x += moveDir * moveSpeed * Time.deltaTime;
+                pos.x = Mathf.Clamp(pos.x, leftLimitX, rightLimitX);
+
+                currentMagicCircle.transform.position = pos;
+
             }
 
+            //Dropping magic circle logic
             if (InputManager.Instance.GetDropInput())
             {
                 DropCurrentCircle();
@@ -79,6 +102,33 @@ public class GameplayController : MonoBehaviour
         GameStateManager.Instance.SetState(GameStateManager.GameState.Dropped);
     }
 
+    private void AppearDropLine()
+    {
+
+        //Make drop line game object active if was inactive
+        if (dropLine.gameObject.activeSelf == false)
+        {
+            dropLine.gameObject.SetActive(true);
+        }
+
+        //Move the drop line along with the current circle
+        dropLine.position = new Vector3(currentMagicCircle.transform.position.x, dropLine.position.y, dropLine.position.z);
+
+    }
+
+    private void DisappearDropLine()
+    {
+        //Deactivate drop line if was active
+        if (dropLine.gameObject.activeSelf == true)
+        {
+            dropLine.gameObject.SetActive(false);
+        }
+
+        //Reset X position
+        dropLine.position = new Vector3(0, dropLine.position.y, dropLine.position.z);
+
+    }
+
     #endregion
 
     #region Game State Logic
@@ -88,6 +138,11 @@ public class GameplayController : MonoBehaviour
         if (state == GameStateManager.GameState.Aiming)
         {
             AskToSpawnNewCircle();
+        }
+        
+        if (state == GameStateManager.GameState.Dropped)
+        {
+            DisappearDropLine();
         }
     }
 
